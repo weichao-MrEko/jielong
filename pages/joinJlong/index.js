b76f030890d84f4b54598b6211d0bfb2495561ba
weichao-MrEko <769016345@qq.com>
2018-07-16 11:39:58 +0800

    7.16

--- pages/joinJlong/index.js
+++ pages/joinJlong/index.js
@@ -28,7 +28,8 @@ Component({
    item_info:'',
    adr_panduan:'',
    address:'',
-   summation:{zongshu:0,jiage:0}
+   summation:{zongshu:0,jiage:[],zongjiage:0},
+   beizhu:''
   
 
   },
@@ -42,6 +43,7 @@ Component({
       that.setData({
         act: JSON.parse(options.act)
       })
+      console.log(that.data.act)
       wx.request({
         url: app.globalData.urlPrefix + 'joinJl/join_jl',
         data: {
@@ -50,7 +52,7 @@ Component({
           theme_id:that.data.act.theme_id
         },
         success: function (res) {
-          console.log(res.data)
+          console.log(res)
           that.setData({
             address_info: res.data.address_info,
             self_info: res.data.self_info,
@@ -77,17 +79,18 @@ Component({
   /**加号 */
     add:function(e){
      this.data.summation.zongshu=0  
-     this.data.summation.jiage = ''
+     this.data.summation.zongjiage =0
       var id = e.currentTarget.dataset.id
       var aa = this.data.item_info[id].may_amount
      
-      if (aa >= this.data.item_info[id].amount){ 
-      }
+      if (aa >= this.data.item_info[id].amount){}
       else{
         this.data.item_info[id].may_amount = aa + 1
         for (var i = 0; i < this.data.item_info.length;i++){         
           this.data.summation.zongshu += this.data.item_info[i].may_amount
-          this.data.summation.jiage = this.data.item_info[id].price * this.data.item_info[id].may_amount
+          this.data.summation.jiage[i] = this.data.item_info[i].price * this.data.item_info[i].may_amount
+          this.data.summation.zongjiage += this.data.summation.jiage[i]
+          console.log(this.data.summation.zongjiage )
         }
         this.setData({
           item_info: this.data.item_info,
@@ -96,11 +99,12 @@ Component({
         })
         
       }
-      console.log(this.data.summation)
+  
     },
     /**减号 */
     reduce: function (e) {
-      this.data.summation=0
+      this.data.summation.zongshu = 0
+      this.data.summation.zongjiage = 0
       console.log(e.currentTarget.dataset.id)
      var that=this
      var id = e.currentTarget.dataset.id
@@ -118,7 +122,8 @@ Component({
           that.data.item_info[id].may_amount = aa-1
           for (var i = 0; i < this.data.item_info.length; i++) {
             this.data.summation.zongshu += this.data.item_info[i].may_amount 
-           
+            this.data.summation.jiage[i] = this.data.item_info[i].price * this.data.item_info[i].may_amount
+            this.data.summation.zongjiage += this.data.summation.jiage[i]
           }
          that.setData({
            item_info: that.data.item_info,
@@ -170,6 +175,28 @@ Component({
             item_info: res.data.item_info,
             adr_panduan: res.data.adr_panduan
           })
+        }
+      })
+    },
+    beizhu:function(e){
+      console.log(e.detail.value)
+      this.setData({
+        beizhu: e.detail.value
+      })
+
+    },
+    zhifu:function(){
+      wx.request({
+        url: app.globalData.urlPrefix+'Joinjl/add_actor',
+        data:{
+          user_id: app.globalData.idda.uid,
+          item:this.data.item_info,
+          price: this.data.summation.zongjiage,
+          amount: this.data.summation.zongshu,
+          desc: this.data.beizhu
+        },
+        success:function(res){
+            console.log(res)
         }
       })
     }
