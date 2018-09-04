73bde5b91cb58d5999472b71d8551a4a3338d4fc
smith <3162547163@qq.com>
2018-09-04 11:16:27 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -192,11 +192,6 @@ Page({
       url: '../comments/comments'
     })
   },
-  yue:function(){
-    wx.navigateTo({
-      url: '../yue/yue',
-    })
-  },
   jlcon: function() {
     var Time = new Date().getTime()
   
